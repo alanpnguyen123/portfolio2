a9acc8d816e41f487aaa17495f89762c3facd937
alanpnguyen123 <alannguyen456@gmail.com>
2022-04-25 18:06:14 -0400

    April 25, 2022 6:06 PM

--- quizapp/screens/HomeScreen.js
+++ quizapp/screens/HomeScreen.js
@@ -1,22 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import {  View, Button, FlatList } from 'react-native';
-import { useCallback } from 'react';
+import * as React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  KeyboardAvoidingView,
+} from 'react-native';
+import { Card, ListItem, Button, Icon } from 'react-native-elements';
 import { styles } from '../App'
 
-
-export default function HomeScreen({ navigation,  route }) {
-    let quizList = route.params.quizList
-    let gotoQuiz = useCallback(({ key }) => {
-      navigation.navigate("MathQuiz", { quizKey: key, count: 0, quizList: quizList })
-      navigation.navigate("Survey", { quizKey: key, count: 0, quizList: quizList })
-    })
-    // let gotoSurvey = useCallback(({ key }) => {
-    return (
-      <View style={styles.container}>
-        <FlatList data={quizList} renderItem={({ item }) =>
-          <Button onPress={() => gotoQuiz(item)} title={item.name}></Button>
-        } />
-        <StatusBar style="auto" />
-      </View>
-    )
+export default class Dashboard extends React.Component {
+  constructor(){
+    super();
+    this.state={
+    }
   }
+
+  render() {
+    return (
+      <KeyboardAvoidingView style={styles.containerBox}>
+        <View>
+          <Text style={styles.textCenter}>Select Quiz</Text>
+        </View>
+        <Button onPress={() => {
+          this.props.navigation.navigate('MathQuiz');
+        }}>
+        <Card>
+          <Card.Title>Natural number</Card.Title>
+          <Card.Divider/>
+           <View Button={styles.user}>
+            <Image
+              style={styles.box}
+              resizeMode="cover"
+            />
+          </View>
+        </Card>
+        </Button>
+      
+
+        <Button onPress={() => {
+          this.props.navigation.navigate('Survey');
+        }}>
+          <Card>
+            <Card.Title>Whole Number</Card.Title>
+            <Card.Divider/>
+            <View style={styles.user}>
+              <Image
+                style={styles.box}
+                resizeMode="cover"
+              />
+            </View>
+          </Card>
+        </Button>
+      </KeyboardAvoidingView>
+    );
+  }
+}
