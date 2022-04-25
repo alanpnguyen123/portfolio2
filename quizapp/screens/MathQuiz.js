b6377a1ddb2c36bf4d179c88b240d50da5066104
alanpnguyen123 <alannguyen456@gmail.com>
2022-04-25 19:07:34 -0400

    April 25, 2022 7:07 PM

--- quizapp/screens/MathQuiz.js
+++ quizapp/screens/MathQuiz.js
@@ -1,39 +1,38 @@
-import * as React from 'react';
 import { styles } from '../App'
+import React, { Component } from 'react';
 
-export default class QuizScreen extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        numberOfGuests: 2
-      };
-  
-      this.handleInputChange = this.handleInputChange.bind(this);
-    }
-  
-    handleInputChange(event) {
-      const target = event.target;
-      const value = target.type === 'checkbox' ? target.checked : target.value;
-      const name = target.name;
-  
-      this.setState({
-        [name]: value
-      });
-    }
-  
-    render() {
-      return (
-        <form>
-          <br />
-          <label>
-            Number of guests:
-            <input
-              name="numberOfGuests"
-              type="number"
-              value={this.state.numberOfGuests}
-              onChange={this.handleInputChange} />
-          </label>
-        </form>
-      );
-    }
+export default class QuizScreen extends Component {
+
+constructor(props){
+super(props);
+
+this.state = {
+  username : ''
 }
+
+this.updateInput = this.updateInput.bind(this);
+this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+
+updateInput(event){
+this.setState({username : event.target.value})
+}
+
+
+handleSubmit(){
+console.log('Your input value is: ' + this.state.username)
+//Send state to the server code
+}
+
+
+
+render(){
+return (
+    <div>
+    <input type="text" onChange={this.updateInput}></input>
+    <input type="submit" onClick={this.handleSubmit} ></input>
+    </div>
+  );
+}
+} 
