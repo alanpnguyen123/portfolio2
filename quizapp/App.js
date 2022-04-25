87881855e4b7a190649615544b785c255050632e
alanpnguyen123 <alannguyen456@gmail.com>
2022-04-25 16:41:09 -0400

    April 25, 2022 4:41 PM

--- quizapp/App.js
+++ quizapp/App.js
@@ -7,24 +7,7 @@ import HomeScreen from './screens/HomeScreen'
 
 
 const Stack = createNativeStackNavigator();
-function RepetitionExerciseScreen({ navigation, route }) {
-  let { exerciseKey, exerciseList } = route.params
-  let [count, setCount] = useState(0)
-  let currentExercise = exerciseList.find(ex => ex.key === exerciseKey)
-  let suggestion = useCallback(() => {
-    navigation.navigate("RepetitionExercise", { exerciseKey: currentExercise.suggestedKey, exerciseList: exerciseList, count: 0 })
-  })
-  return (
-    <View style={styles.container}>
-      <Text>{currentExercise.name}: {count}</Text>
-      <Button onPress={() => setCount(count => count + 1)} title="Finish Rep"></Button>
-      <Button onPress={() => navigation.navigate("Home")} title="Return Home"></Button>
-      <Button onPress={() => setCount(0)} title="Reset Reps"></Button>
-      <Button onPress={suggestion} title="Suggest New Exercise"></Button>
-      <StatusBar style="auto" />
-    </View>
-  )
-}
+
 
 export default function App() {
   let exerciseList = [
