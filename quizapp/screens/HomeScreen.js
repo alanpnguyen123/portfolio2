59bca96204c564669e5db4c83fa28a37d94c470d
alanpnguyen123 <alannguyen456@gmail.com>
2022-04-25 18:09:23 -0400

    April 25, 2022 6:09 PM

--- quizapp/screens/HomeScreen.js
+++ quizapp/screens/HomeScreen.js
@@ -1,61 +1,22 @@
-import * as React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  TextInput,
-  TouchableOpacity,
-  Alert,
-  KeyboardAvoidingView,
-} from 'react-native';
-import { Card, ListItem, Button, Icon } from 'react-native-elements';
+import { StatusBar } from 'expo-status-bar';
+import {  View, Button, FlatList } from 'react-native';
+import { useCallback } from 'react';
 import { styles } from '../App'
 
-export default class Dashboard extends React.Component {
-  constructor(){
-    super();
-    this.state={
-    }
-  }
 
-  render() {
+export default function HomeScreen({ navigation,  route }) {
+    let quizList = route.params.quizList
+    let gotoQuiz = useCallback(({ key }) => {
+      navigation.navigate("MathQuiz", { quizKey: key, count: 0, quizList: quizList })
+      navigation.navigate("Survey", { quizKey: key, count: 0, quizList: quizList })
+    })
+    // let gotoSurvey = useCallback(({ key }) => {
     return (
-      <KeyboardAvoidingView style={styles.containerBox}>
-        <View>
-          <Text style={styles.textCenter}>Select Quiz</Text>
-        </View>
-        <Button onPress={() => {
-          this.props.navigation.navigate('MathQuiz');
-        }}>
-        <Card>
-          <Card.Title>Natural number</Card.Title>
-          <Card.Divider/>
-           <View Button={styles.user}>
-            <Image
-              style={styles.box}
-              resizeMode="cover"
-            />
-          </View>
-        </Card>
-        </Button>
-      
-
-        <Button onPress={() => {
-          this.props.navigation.navigate('Survey');
-        }}>
-          <Card>
-            <Card.Title>Whole Number</Card.Title>
-            <Card.Divider/>
-            <View style={styles.user}>
-              <Image
-                style={styles.box}
-                resizeMode="cover"
-              />
-            </View>
-          </Card>
-        </Button>
-      </KeyboardAvoidingView>
-    );
+      <View style={styles.container}>
+        <FlatList data={quizList} renderItem={({ item }) =>
+          <Button onPress={() => gotoQuiz(item)} title={item.name}></Button>
+        } />
+        <StatusBar style="auto" />
+      </View>
+    )
   }
-}
