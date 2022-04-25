ec530117458b966cd94f246edc3acd2bae41dd8d
alanpnguyen123 <alannguyen456@gmail.com>
2022-04-25 17:08:15 -0400

    April 25, 2022 5:08 PM

--- quizapp/screens/HomeScreen.js
+++ quizapp/screens/HomeScreen.js
@@ -6,13 +6,13 @@ import { styles } from '../App'
 
 export default function HomeScreen({ navigation,  route }) {
     let quizList = route.params.quizList
-    let gotoExercise = useCallback(({ key }) => {
-      navigation.navigate("RepetitionExercise", { quizKey: key, count: 0, quizList: quizList })
+    let gotoQuiz = useCallback(({ key }) => {
+      navigation.navigate("MathQuiz", { quizKey: key, count: 0, quizList: quizList })
     })
     return (
       <View style={styles.container}>
         <FlatList data={quizList} renderItem={({ item }) =>
-          <Button onPress={() => gotoExercise(item)} title={item.name}></Button>
+          <Button onPress={() => gotoQuiz(item)} title={item.name}></Button>
         } />
         <StatusBar style="auto" />
       </View>
