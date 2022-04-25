54bbe0707a26baef7c128a35bbd539e60b2fa589
alanpnguyen123 <alannguyen456@gmail.com>
2022-04-25 19:55:23 -0400

    April 25, 2022 7:55 PM

--- quizapp/screens/MathQuiz.js
+++ quizapp/screens/MathQuiz.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FlatList, Text, TextInput, View } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Button, Card } from 'react-native-elements';
 import { styles } from '../App'
 
 let questions = [
@@ -20,12 +20,16 @@ const QuizScreen = () => {
   const [text, setText] = useState('');
   return (
     <View style={styles.container}>
-         <Text>Type in the best Answer</Text>
-      <TextInput
-        placeholder="Type Your Answer Here"
-        onChangeText={newText => setText(newText)}
-        defaultValue={text}
-      />
+    <Text>Type in the best Answer</Text>
+        <Card>
+        <View>
+            <TextInput
+            placeholder="Type Your Answer Here"
+            onChangeText={newText => setText(newText)}
+            defaultValue={text}
+            />
+        </View>
+        </Card>
       <Button title="Submit" disabled={true}></Button>
     </View>
   );
