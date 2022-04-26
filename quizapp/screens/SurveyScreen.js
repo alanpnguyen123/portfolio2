ce2a6ae26920cf2c0f331478e26222f5503c73f2
alanpnguyen123 <alannguyen456@gmail.com>
2022-04-25 21:44:24 -0400

    April 25, 2022 9:44 PM

--- quizapp/screens/SurveyScreen.js
+++ quizapp/screens/SurveyScreen.js
@@ -10,6 +10,17 @@ let survey = [
   ]
 const SurveyScreen = () => {
     let [text, setText] = useState('');
+    let [score, setScore] = useState()
+    let checkAnswers = useCallback (()=>{
+        if (text == 1-10){
+            setScore("Thank you for the feedback!")  
+            console.log(text)     
+        }
+        else{
+            setScore("Please enter a number 1 through 10")
+            console.log(text)
+        }
+    },[text,score])
     
   return (
     <View style={styles.container}>
@@ -22,7 +33,7 @@ const SurveyScreen = () => {
             onChangeText={newText => setText(newText)}
             defaultValue={text}
             />
-            <Button title="Submit" onPress={()=><Text>Your Feedback has been accepted</Text>} disabled={text ===""}></Button>
+            <Button title="Submit" onPress={()=>checkAnswers()} disabled={text ===""}></Button>
             
         </View>
         </Card>
